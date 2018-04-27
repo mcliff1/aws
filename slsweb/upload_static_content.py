28b17bcc2d934705e928190258891dd3ad178afe
Matt Cliff <matt@denvercliffs.com>
2018-04-27 15:37:43 +0000

    added SNS topic, code build, and lambda deploy hook

--- slsweb/upload_static_content.py
+++ slsweb/upload_static_content.py
@@ -3,30 +3,35 @@ import boto3
 from io import BytesIO
 import zipfile
 
-
-
 def lambda_handler(event, context):
     """
     Downloads zip from build bucket, and extracts to deploy bucket
     """
-    
-    s3 = boto3.resource('s3')
-    build_bucket = s3.Bucket(os.environ['SOURCE_BUCKET'])
-    deploy_bucket = s3.Bucket(os.environ['TARGET_BUCKET'])
+
+    sns = boto3.resource('sns')
+    topic = sns.Topic(os.environ['SNS_TOPIC'])
+
+    try:    
+        s3 = boto3.resource('s3')
+        build_bucket = s3.Bucket(os.environ['SOURCE_BUCKET'])
+        deploy_bucket = s3.Bucket(os.environ['TARGET_BUCKET'])
+
+        if not zip_fileref:
+            zip_fileref = BytesIO()
+        build_bucket.download_fileobj(os.environ['BUILD_PACKAGE'], zip_fileref)
+
+        # now upload
+        with zipfile.ZipFile(zip_fileref) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                deploy_bucket.upload_fileobj(obj, nm)
+                deploy_bucket.Object(nm).Acl().put(ACL='public-read')
 
 
-    #build_zip = StringIO.StringIO()
-    if not zip_fileref:
-        zip_fileref = BytesIO()
-    build_bucket.download_fileobj('slsbuild.zip', zip_fileref)
-
-    # now upload
-    with zipfile.ZipFile(zip_fileref) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            deploy_bucket.upload_fileobj(obj, nm)
-            deploy_bucket.Object(nm).Acl().put(ACL='public-read')
-
+        topic.publish(Subject="SLS Portfolio Deployed", Message="Portfolio successfully deployed")
+    except:
+        topic.publish(Subject="SLS Deployment Failed", Message="Portfolio successfully deployed")
+        raise
     
     return 'Copied Code from one bucket to another'
 
