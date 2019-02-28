2c480af98c484bc0fc2b5b91378e2b3fd9e9cbe8
Matt Cliff <matt@denvercliffs.com>
2019-02-28 22:45:04 +0000

    update s3 filetype prefix

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -2,7 +2,7 @@
 date
 ls
 # if the bucket exists list it, else make it
-aws s3 ls $CI_BUCKET || aws s3 mb $CI_BUCKET
+aws s3 ls $CI_BUCKET || aws s3 mb s3://$CI_BUCKET
 aws sts get-caller-identity
 
 echo "Stack name is $STACK_NAME"
