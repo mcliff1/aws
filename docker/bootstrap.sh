e23a473a1d04d1fe81c3efb68a741c4183b476f0
Matt Cliff <matt@denvercliffs.com>
2019-02-28 22:43:40 +0000

    updates on bootstrap

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 date
 ls
-aws s3 ls
+# if the bucket exists list it, else make it
+aws s3 ls $CI_BUCKET || aws s3 mb $CI_BUCKET
 aws sts get-caller-identity
 
+echo "Stack name is $STACK_NAME"
+
+echo "params are $@"
+
 # run aws create-stack with / ???
+# check to see if the bucket exists
 
 #  or maybe instead run SAM command ???
 sam
